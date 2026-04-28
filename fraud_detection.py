23df29e69504f7dbf6ebf38f3fb53d5ba58d044c
Abdillahiomar <36307550+Abdillahiomar@users.noreply.github.com>
2026-04-28 08:53:07 +0300

    Filter completed transactions in fraud detection
    We will use only the completed transactions

--- fraud_detection.py
+++ fraud_detection.py
@@ -28,6 +28,7 @@ if uploaded_file:
         df['DEBIT_MSISDN'] = df['DEBIT_MSISDN'].str.strip()
         df['CREDIT_MSISDN'] = df['CREDIT_MSISDN'].str.strip()
         df['DATE'] = df['INITATE_DATE'].dt.date
+        df = df[df['TRANS_STATUS'].str.strip().str.upper() == 'COMPLETED'].reset_index(drop=True)
         
         # Créer des indexes pour accélérer les filtres
         df = df.sort_values('INITATE_DATE').reset_index(drop=True)
